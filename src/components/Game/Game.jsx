f5e694c990c0508758612f5fce7f74a2d64021cd
ahmad <72674221+WhereDidMyDivGo@users.noreply.github.com>
2025-06-25 00:32:51 +0400

    Enhance Game component: implement random card selection and display, add slot machine effect for card reveal

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,6 +1,44 @@
 import "./Game.css";
 
+import Rock from "../PickOptions/Rock";
+import Paper from "../PickOptions/Paper";
+import Scissors from "../PickOptions/Scissors";
+import { useState, useEffect } from "react";
+
 function Game({ className, chosenCard }) {
+  const cards = [<Rock />, <Paper />, <Scissors />];
+  const [randomCard, setRandomCard] = useState(null);
+
+  useEffect(() => {
+    if (!chosenCard) return;
+    setTimeout(() => {
+      let count = 0;
+      let interval;
+      function rollTheSlotMachine() {
+        randomShuffle();
+        count++;
+        if (count >= 40) {
+          clearTimeout(interval);
+          return;
+        }
+        // increase delay exponentially as count increases
+        const progress = count / 40;
+        const delay = 30 + (400 - 30) * Math.pow(progress, 2.5);
+        interval = setTimeout(rollTheSlotMachine, delay);
+      }
+      rollTheSlotMachine();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [chosenCard]);
+
+  function randomShuffle() {
+    const rand = Math.floor(Math.random() * cards.length);
+    setRandomCard(cards[rand]);
+  }
+
+  const winner = getWinner(chosenCard, randomCard);
+
   return (
     <div className={`gameDiv ${className}`}>
       <div className="you-picked">
@@ -10,7 +48,7 @@ function Game({ className, chosenCard }) {
 
       <div className="house-picked">
         <h1>THE HOUSE PICKED</h1>
-        <div className="circle"></div>
+        <div className="circle">{randomCard}</div>
       </div>
     </div>
   );
