f608fab4dad142c706611023d7c1c506dd32f243
ahmad <72674221+WhereDidMyDivGo@users.noreply.github.com>
2025-06-25 18:54:59 +0400

    Refactor Rules component: update structure and styles for rules button and modal, enhance click handling for modal visibility

--- src/components/Rules/Rules.jsx
+++ src/components/Rules/Rules.jsx
@@ -1,10 +1,38 @@
 import "./Rules.css";
 
+import closeIcon from "../../assets/images/icon-close.svg";
+import rulesImage from "../../assets/images/image-rules.svg";
+
+import { useState, useRef } from "react";
+
 function Rules() {
+  const [openRules, setOpenRules] = useState(false);
+  const modalRef = useRef(null);
+
+  function clickHandler(e) {
+    if (e.target === e.currentTarget) {
+      setOpenRules(false);
+    }
+  }
+
   return (
-    <button className="rules">
-      <p>RULES</p>
-    </button>
+    <div className="rulesDiv">
+      <button className="rules-button" onClick={() => setOpenRules(true)}>
+        <p>RULES</p>
+      </button>
+
+      <div className={`modal-wrapper ${openRules ? "" : "hidden"}`} onClick={clickHandler}>
+        <div className="rules-modal" ref={modalRef}>
+          <div className="rules-title">
+            <h1>RULES</h1>
+            <button onClick={() => setOpenRules(false)}>
+              <img src={closeIcon} />
+            </button>
+          </div>
+          <img className="rules-image" src={rulesImage} />
+        </div>
+      </div>
+    </div>
   );
 }
 
